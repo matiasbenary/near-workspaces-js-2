6a6f7c8beb6b370aefa42dc157741b988275324f
matiasbenary <matiasbenary@gmail.com>
2023-11-29 12:10:09 +0000

    Deploying to gh-pages from @ matiasbenary/near-workspaces-js@109658ad3decee673401df7a2ffeb525ac3e44ea 🚀

--- packages/js/src/account/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './near-account';
-export * from './near-account-manager';
-export * from './account-manager';
-export * from './account';
--- packages/js/src/server/node-port-check.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'node-port-check' {
-  interface NodePortCheck {
-    nextAvailable: (from: number, ipAddr: string) => Promise<number>;
-  }
-
-  const NodePortCheck: NodePortCheck;
-
-  export = NodePortCheck;
-}
